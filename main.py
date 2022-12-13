fe1ef9a9cf8eb1d1112dda35a56c337afa9286f8
aloma123 <65070190@kmitl.ac.th>
2022-12-14 00:17:16 +0700

    update

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 import pygame, sys
-from settinggame import *
-from debug import *
+from settings import *
 from level import Level
-from tie import *
-from tree import * 
 
 class Game:
 	def __init__(self):
 
+		# general setup
 		pygame.init()
-		self.screen = pygame.display.set_mode((width, heigth))
-		pygame.display.set_caption("Farmmer's Adventure")
+		self.screen = pygame.display.set_mode((900,600))
+		pygame.display.set_caption('Zelda')
 		self.clock = pygame.time.Clock()
 
 		self.level = Level()
+
+		# sound 
+		main_sound = pygame.mixer.Sound('../audio/main.ogg')
+		main_sound.set_volume(0.5)
+		main_sound.play(loops = -1)
+	
 	def run(self):
 		while True:
 			for event in pygame.event.get():
-
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_m:
+						self.level.toggle_menu()
 
-			self.screen.fill('black')
+			self.screen.fill(WATER_COLOR)
 			self.level.run()
 			pygame.display.update()
-			self.clock.tick(fps)
+			self.clock.tick(FPS)
+
 if __name__ == '__main__':
 	game = Game()
 	game.run()
